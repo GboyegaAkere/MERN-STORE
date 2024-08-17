e3d583fa6d31a6f05a8ca07e4c66ee1ed74334f8
Gboyega Daniel <119269023+GboyegaAkere@users.noreply.github.com>
2024-08-17 16:04:44 +0100

    working on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className='h-16 shadow-md'>
+        <div className='container mx-auto h-full flex items-center px-4 justify-between'>
+            <div className=''>
+                <h1>MY LOGO</h1>
+            </div>
+
+            <div className=''>
+                <h1>MY LOGO</h1>
+            </div>
+
+            <div className=''>
+                <h1>MY LOGO</h1>
+            </div>
+        </div>
+    </header>
   )
 }
 
