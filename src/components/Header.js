c97f8b12dfcaa8a5fb02a1cc202110f763cb5016
Gboyega Daniel <119269023+GboyegaAkere@users.noreply.github.com>
2024-08-18 09:39:12 +0100

    worked on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import { IoSearchOutline } from "react-icons/io5";
 
 function Header() {
   return (
     <header className='h-16 shadow-md'>
         <div className='container mx-auto h-full flex items-center px-4 justify-between'>
-            <div className=''>
+            <div className='cursor-pointer'>
                 <h1>MY LOGO</h1>
             </div>
 
-            <div className=''>
-                <h1>MY LOGO</h1>
+            <div className='hidden lg:flex items-center w-full max-w-sm h-10 rounded-full justify-between border focus-within:shadow-md'>
+               <input type='text' placeholder='search product here...' className='w-full outline-none pl-10'/>
+               <div className='text-lg h-full min-w-[50px] flex items-center justify-center bg-red-400  rounded-r-full'>
+                <IoSearchOutline />
+               </div>
             </div>
 
             <div className=''>
